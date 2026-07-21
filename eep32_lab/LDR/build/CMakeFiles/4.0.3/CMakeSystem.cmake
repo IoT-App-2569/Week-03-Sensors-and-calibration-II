fd86522b6a8b4c995de6d38acd3a9b99940d6d0d
Pleumjuniors <67030172@kmitl.ac.th>
2026-07-21 09:47:28 +0700

    67030172

---
+++ eep32_lab/LDR/build/CMakeFiles/4.0.3/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Windows-10.0.26200")
+set(CMAKE_HOST_SYSTEM_NAME "Windows")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.26200")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
+
+include("C:/Users/Pleum Juniors/Week-03-Sensors-and-calibration-II/eep32_lab/LDR/build/toolchain/toolchain-esp32.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
